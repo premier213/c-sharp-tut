b014439938233f6412769f3a5464b2751f8ba119
premier213 <premierp1213@gmail.com>
2023-02-12 00:37:11 +0330

    initialize

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte numner=2;
+            int count = 256;
+            bool hasCount = false;
+            float dec = 3.25f;
+            char character = 'a';
+            string firstName = "meysam";
+            Console.WriteLine(numner);
+            Console.WriteLine(count);
+            Console.WriteLine(hasCount);
+            Console.WriteLine(dec);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+        }
+    }
+}
